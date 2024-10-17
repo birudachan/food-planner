42baa18c3ee79d41f25dd43e293b473fb15b3ad1
tnanhd <tnanh.dev@gmail.com>
2024-10-17 17:11:38 +0700

    Feat: Add Search View and Ingredient Recycler View

---
+++ app/src/main/java/app/twentyhours/foodplanner/CustomAdapter.kt
@@ -0,0 +1,32 @@
+package app.twentyhours.foodplanner
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter(private val dataSet: Array<Int>) :
+    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.home_ingredient_rc_item_image_view)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.home_ingredient_rc_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.imageView.setImageResource(dataSet[position])
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+}
--- app/src/main/java/app/twentyhours/foodplanner/MainActivity.kt
+++ app/src/main/java/app/twentyhours/foodplanner/MainActivity.kt
@@ -1,10 +1,15 @@
 package app.twentyhours.foodplanner
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        this.findViewById<Button>(R.id.home_setting_button).setOnClickListener {
+            Toast.makeText(this, "Setting button clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        this.findViewById<ImageView>(R.id.home_profile_image_view).setOnClickListener {
+            Toast.makeText(this, "Profile image clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        val dataset = arrayOf(
+            R.drawable.beef,
+            R.drawable.chicken,
+            R.drawable.shrimp,
+            R.drawable.fish,
+            R.drawable.beef,
+            R.drawable.chicken,
+            R.drawable.shrimp,
+            R.drawable.fish
+        )
+        val customAdapter = CustomAdapter(dataset)
+
+        val recyclerView:
+                RecyclerView = findViewById(R.id.home_ingredient_recycler_view)
+        recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = customAdapter
     }
 }
